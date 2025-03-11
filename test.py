b435786466609506f743fe9ee7b2f00c2cd9ef1c
STV <88960155+StvMy@users.noreply.github.com>
2025-03-11 19:41:19 +0800

    Delete test.py
    this just file for me to test code

--- test.py
+++
@@ -1,9 +0,0 @@
-import comtypes.client as comt
-
-word = comt.CreateObject('Word.Application')
-
-doc = word.Documents.Open("D:\\CODE\\1 test.docx")
-doc.SaveAs("D:/CODE/output.pdf", FileFormat=17)  # 17 = PDF format
-doc.Close()
-word.Quit()
-
